d86460ecd0586e2c24c04113b1cb3f0385c760e8
Bar Melinarskiy <bar.m@skywind.com>
2024-10-29 20:54:26 +0100

    update

--- Source/analysis.py
+++ Source/analysis.py
@@ -6,6 +6,7 @@ Student 3: Mohammed Bashabeeb, student number: 7060424
 Date: September 12, 2024
 Description: Assignment 2 - Classification for the Detection of Opinion Spam - analysis for report
 """
+from fileinput import filename
 
 import numpy as np
 import pandas as pd
@@ -14,65 +15,111 @@ from scipy.optimize import differential_evolution
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 from wordcloud import WordCloud
-
 from utils import get_df, FAKE, EVALUATIONS_FILENAME
 
 
-def create_word_cloud(words: str, title: str):
+def create_word_cloud(words: str, filename: str, title: str, colored_word=None, color=None):
     """
-    Plot word cloud on given dataframe
+    Plot word cloud with an optional colored word in the title.
 
     Parameters:
     -----------
-    df : str a string of list of words
-    title : str, title for the word cloud plot
+    words : str
+        A string of words to generate the word cloud.
+    filename : str
+        Filename to save the plot.
+    title : str
+        Title for the word cloud plot.
+    colored_word : str, optional
+        Word to color differently in the title.
+    color : str, optional
+        Color of the colored_word.
 
     Returns:
     --------
     None
     """
     # Create and configure word cloud
-    wordcloud = WordCloud( width=1000,
-                           height=1000,
-                           background_color='white',
-                           colormap='magma',
-                           max_words=5000,
-                           random_state=42).generate(words)
+    wordcloud = WordCloud(width=1000,
+                          height=1000,
+                          background_color='white',
+                          colormap='magma',
+                          max_words=5000,
+                          random_state=42).generate(words)
 
     # Plot the word cloud
     plt.figure(figsize=(10, 5))
-    plt.clf()
-    plt.imshow(wordcloud)
+    plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis('off')
-    plt.title(title, fontsize=12, fontweight='bold', loc='center')
-    plt.savefig(f"../Output/{title}.png")
-    plt.show()
 
+    # Display title with colored word
+    if colored_word and color:
+        # Split title around colored word
+        title_parts = title.split(colored_word)
+
+
+        x = [0.55, 0.6, 0.655]
+        if colored_word == 'truthful':
+            x = [0.5, 0.6, 0.705]
+
+        if 'Top' in title:
+            x = [x_i + 0.14 for x_i in x]
+
+
+        # Plot the first part of the title
+        plt.text(x[0], 1.05, title_parts[0], fontsize=14, fontweight='bold', ha='right', transform=plt.gca().transAxes)
+
+        # Plot the colored word in the middle
+        plt.text(x[1], 1.05, colored_word, fontsize=14, fontweight='bold', color=color, ha='center',
+                 transform=plt.gca().transAxes)
+
+        # Plot the rest of the title after the colored word
+        plt.text(x[2], 1.05, title_parts[1], fontsize=14, fontweight='bold', ha='left', transform=plt.gca().transAxes)
+    else:
+        plt.title(title, fontsize=14, fontweight='bold')
+
+    # Save and show the plot
+    plt.savefig(f"../Output/{filename}.png")
+    plt.show()
 
 
 def plot_word_clouds():
     """
-    Plot word cloud on entire fake reviews text, and on top 5 words from the different models
+    Plot word cloud on entire fake reviews text, and on top 5 words from the different models.
     """
-
-    ## plot word cloud for entire fake review dataset
+    # Load dataset
     df = get_df()
+
+    # Plot word cloud for entire fake review dataset
     fake_reviews = df[df['is_fake'] == FAKE.DECEPTIVE]['review']
     words = " ".join(review.lower() for review in fake_reviews.dropna())
-    create_word_cloud(words, title="Most frequent words in fake reviews dataset")
+    create_word_cloud(words, filename='word_cloud_fake',
+                      title="Most frequent words in fake reviews dataset", colored_word="fake", color="red")
 
-    ## plot word cloud for entire truthful review dataset
-    df = get_df()
+    # Plot word cloud for entire truthful review dataset
     truthful_reviews = df[df['is_fake'] == FAKE.TRUTHFUL]['review']
     truthful_words = " ".join(review.lower() for review in truthful_reviews.dropna())
-    create_word_cloud(truthful_words, title="Most frequent words in truthful reviews dataset")
+    create_word_cloud(truthful_words, filename='word_cloud_truthful',
+                      title="Most frequent words in truthful reviews dataset", colored_word="truthful", color="green")
 
-    ## plot word cloud for top 5 words from models
-    df_evaluations = pd.read_csv(EVALUATIONS_FILENAME)
-    top_5_features_deceptive = df_evaluations['top_5_features_deceptive']
-    top_5_words = " ".join(
-        word.strip() for top_words in top_5_features_deceptive.dropna() for word in top_words.split(','))
-    create_word_cloud(top_5_words, title="Top 5 terms relating to fake reviews across all models")
+    # Plot word cloud for top 5 words from models
+    df_top_k_features = pd.read_csv("../Output/top_k_features.csv")
+
+    filtered_values_deceptive = np.where((df_top_k_features['class'] ==  1))
+    top_5_features_deceptive = np.array(df_top_k_features.loc[filtered_values_deceptive]['feature'])
+    top_5_features_deceptive_str = ' '.join(top_5_features_deceptive)
+    create_word_cloud(top_5_features_deceptive_str, filename='word_cloud_top_deceptive',
+                      title="Top features predicting fake reviews",
+                      colored_word="fake", color="red")
+
+
+    filtered_values_truthful = np.where((df_top_k_features['class'] == 0))
+    top_5_features_truthful = np.array(df_top_k_features.loc[filtered_values_truthful]['feature'])
+    top_5_features_truthful_str = ' '.join(top_5_features_truthful)
+    create_word_cloud(top_5_features_truthful_str, filename='word_cloud_top_truthful',
+                      title="Top features predicting truthful reviews",
+                      colored_word="truthful", color="green")
+
 
 
 def plot_confusion_matrix(y_true, y_pred, title):
@@ -149,7 +196,7 @@ def plot_scores_as_bars(df_scores):
     plt.show()
 
 
-def plot_scores_as_lines(df_scores):
+def plot_bnm_scores_as_lines(df_scores):
     """
     Plot scores as a multi-line plot on given dataframe
 
@@ -207,10 +254,12 @@ def plot_scores_as_lines(df_scores):
 
 
 if __name__ == "__main__":
-    # plot_word_clouds()
-
     df_evaluations = pd.read_csv(EVALUATIONS_FILENAME)
-    plot_scores_as_lines(df_evaluations)
+
+    # # create word cloud from entire dataset and from top features in LR
+    # plot_word_clouds()
+    # # create plot for comparing different n-grams, and # of features in Multinomial Naive Bayes
+    # plot_bnm_scores_as_lines(df_evaluations)
 
     print("Done with plots")
 
